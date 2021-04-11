c50959c3aa976d070e218e342d64f9a4afe7c5db
彭伟 <pengweiqhca@sina.com>
2021-04-11 18:27:06 +0800

    Add MessagePack

--- test/NetCache.SerializerTests/Address.cs
+++ test/NetCache.SerializerTests/Address.cs
@@ -1,13 +1,18 @@
-﻿using ProtoBuf;
+﻿using MessagePack;
+using ProtoBuf;
 
 namespace NetCache.SerializerTests
 {
     [ProtoContract]
-    class Address
+    [MessagePackObject]
+    public class Address
     {
         [ProtoMember(1)]
+        [Key(0)]
         public string? Line1 { get; set; }
+
         [ProtoMember(2)]
+        [Key(1)]
         public string? Line2 { get; set; }
     }
 }
--- test/NetCache.SerializerTests/BinarySerializerTest.cs
+++ test/NetCache.SerializerTests/BinarySerializerTest.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using Hyper;
+﻿using Hyper;
+using MessagePack;
+using System;
 using Xunit;
 
 namespace NetCache.SerializerTests
 {
-    public class ProtobufSerializerTest
+    public class BinarySerializerTest
     {
         [Fact]
         public void GoogleProtobufTest()
         {
             var serializer = new GoogleProtobufSerializer();
 
-            Assert.Empty(serializer.Serialize((MultiplyRequest?) null).ToArray());
+            Assert.Empty(serializer.Serialize((MultiplyRequest?)null).ToArray());
 
             var obj = new MultiplyRequest { X = 3, Y = 5 };
 
@@ -33,7 +34,7 @@ namespace NetCache.SerializerTests
         {
             var serializer = new ProtobufNetSerializer();
 
-            Assert.Empty(serializer.Serialize((Address?) null).ToArray());
+            Assert.Empty(serializer.Serialize((Address?)null).ToArray());
 
             Assert.Equal(3, serializer.Deserialize<int>(serializer.Serialize(3)));
 
@@ -50,6 +51,34 @@ namespace NetCache.SerializerTests
         public void ProtobufNetErrorTest()
         {
             Assert.Throws<InvalidOperationException>(() => new ProtobufNetSerializer().Serialize(new MultiplyRequest()));
+        }
+
+        [Fact]
+        public void MessagePackTest()
+        {
+            var serializer = new MessagePackSerializer();
+
+            Assert.Equal(new byte[] { 192 }, serializer.Serialize((Address?)null).ToArray());
+
+            Assert.Equal(3, serializer.Deserialize<int>(serializer.Serialize(3)));
+
+            var source = new Address { Line1 = "abc", Line2 = "aaaaa" };
+
+            var target = serializer.Deserialize<Address>(serializer.Serialize(source))!;
+
+            Assert.NotNull(target);
+            Assert.Equal(source.Line1, target.Line1);
+            Assert.Equal(source.Line2, target.Line2);
+        }
+
+        [Fact]
+        public void MessagePackErrorTest()
+        {
+#if NET46
+            Assert.Throws<FormatterNotRegisteredException>(() => new MessagePackSerializer().Serialize(new MultiplyRequest()));
+#else
+            Assert.Throws<MessagePackSerializationException>(() => new MessagePackSerializer().Serialize(new MultiplyRequest()));
+#endif
         }
     }
 }
